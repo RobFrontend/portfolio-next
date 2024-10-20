bd1ac0f199456e2be4a316e30dc33407771b7740
RobFrontend <robert.grabowski97@gmail.com>
2024-10-20 18:21:05 +0200

    home client

---
+++ app/components/HomeClientCarousel.js
@@ -0,0 +1,137 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import JeszczeStrona from "@/public/JeszczeStrona.webp";
+import BalanceBook from "@/public/BalanceBook.webp";
+import Biceps from "@/public/Biceps.webp";
+import Robson from "@/public/Robson.webp";
+import { useEffect, useState } from "react";
+
+function HomeClientCarousel() {
+  const [isPercent, setIsPercent] = useState(0);
+  const [isHover, setIsHover] = useState(false);
+
+  const button1BG =
+    isPercent === 0
+      ? `bg-[#575858] hover:bg-[#ddd]`
+      : `bg-[#eee] hover:bg-[#ddd]`;
+  const button2BG =
+    isPercent === -25
+      ? `bg-[#575858] hover:bg-[#ddd]`
+      : `bg-[#eee] hover:bg-[#ddd]`;
+  const button3BG =
+    isPercent === -50
+      ? `bg-[#575858] hover:bg-[#ddd]`
+      : `bg-[#eee] hover:bg-[#ddd]`;
+  const button4BG =
+    isPercent === -75
+      ? `bg-[#575858] hover:bg-[#ddd]`
+      : `bg-[#eee] hover:bg-[#ddd]`;
+
+  useEffect(() => {
+    if (!isHover) {
+      const interval = setInterval(() => {
+        if (isPercent > -75) setIsPercent((p) => p - 25);
+        if (isPercent <= -75) setIsPercent((p) => (p = 0));
+      }, 4000);
+      return () => clearInterval(interval);
+    }
+  }, [isPercent, isHover]);
+
+  return (
+    <div className="overflow-x-hidden w-[75%] max-sm:w-[95%] ">
+      <div
+        className="flex w-[400%] transition-all duration-500 justify-center justify-items-center"
+        style={{ transform: `translateX(${isPercent}%)` }}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+        <div className="w-screen grid p-4">
+          <h3 className="text-3xl opacity-90 text-center pb-6 max-sm:text-xl">
+            JeszczeStronaAlboSto
+          </h3>
+          <Link
+            href="https://www.jeszczestronaalbosto.pl/"
+            target="_blank"
+            className="justify-self-center"
+          >
+            <Image
+              src={JeszczeStrona}
+              alt="Client page"
+              className="justify-self-center"
+            />
+          </Link>
+        </div>
+        <div className=" w-screen grid p-4">
+          <h3 className="text-3xl opacity-90 text-center pb-6 max-sm:text-xl">
+            BalanceBook
+          </h3>
+          <Link
+            href="https://www.balancebook.pl/"
+            target="_blank"
+            className="justify-self-center"
+          >
+            <Image
+              src={BalanceBook}
+              alt="Client page"
+              className="justify-self-center"
+            />
+          </Link>
+        </div>
+        <div className=" w-screen grid p-4">
+          <h3 className="text-3xl opacity-90 text-center pb-6 max-sm:text-xl">
+            Biceps
+          </h3>
+          <Link
+            href="https://www.odzywki-legionowo.pl/"
+            target="_blank"
+            className="justify-self-center"
+          >
+            <Image
+              src={Biceps}
+              alt="Client page"
+              className="justify-self-center"
+            />
+          </Link>
+        </div>
+        <div className="w-screen grid p-4">
+          <h3 className="text-3xl opacity-90 text-center pb-6 max-sm:text-xl">
+            Photobooth360
+          </h3>
+          <Link
+            href="https://robson-fotobudka360.pl/"
+            target="_blank"
+            className="justify-self-center"
+          >
+            <Image
+              src={Robson}
+              alt="Client page"
+              className="justify-self-center"
+            />
+          </Link>
+        </div>
+      </div>
+      <div className="p-8 flex gap-8 justify-center max-sm:gap-4 max-sm:p-3">
+        <button
+          className={`p-4 ${button1BG} rounded-full drop-shadow-lg transition-all duration-300 max-sm:p-3`}
+          onClick={() => setIsPercent(0)}
+        ></button>
+        <button
+          className={`p-4 ${button2BG} rounded-full drop-shadow-lg transition-all duration-300 max-sm:p-3`}
+          onClick={() => setIsPercent(-25)}
+        ></button>
+        <button
+          className={`p-4 ${button3BG} rounded-full drop-shadow-lg transition-all duration-300 max-sm:p-3`}
+          onClick={() => setIsPercent(-50)}
+        ></button>
+        <button
+          className={`p-4 ${button4BG} rounded-full drop-shadow-lg transition-all duration-300 max-sm:p-3`}
+          onClick={() => setIsPercent(-75)}
+        ></button>
+      </div>
+    </div>
+  );
+}
+
+export default HomeClientCarousel;
--- app/page.js
+++ app/page.js
@@ -11,17 +11,19 @@ import AboutNode from "@/public/AboutNode.png";
 import AboutTailwind from "@/public/AboutTailwind.png";
 import AboutGit from "@/public/AboutGit.png";
 import AboutSupabase from "@/public/AboutSupabase.png";
+import HomeClientCarousel from "./components/HomeClientCarousel";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Features />
+      {/* ABOUT */}
       <section className="max-w-[1200px] mx-auto">
         <h2 className="text-4xl tracking-[-4px] pb-6">About me</h2>
         <div className="grid grid-cols-2 gap-8 content-center justify-center justify-items-center items-center px-4 max-md:grid-cols-1">
           <div className="grid">
-            <p className="pb-6 text-justify">
+            <p className="pb-6 text-justify max-sm:text-sm">
               I specialize in frontend development with a strong command of
               HTML, CSS, JavaScript, and React. Currently, I&apos;m enhancing my
               skills in Next.js and Tailwind CSS to build dynamic and responsive
@@ -35,7 +37,7 @@ export default function Home() {
             </p>
             <Link
               href="/about"
-              className="text-xl border-b-[1px] border-[#575858] py-2 px-4 max-md:justify-self-center w-fit"
+              className="text-xl border-b-[1px] border-[#575858] py-2 px-4 max-md:justify-self-center w-fit max-[380px]:max-sm:text-lg"
             >
               More about me
             </Link>
@@ -86,6 +88,63 @@ export default function Home() {
               alt="logo"
               className="max-h-20 max-w-20 brightness-90 grayscale-[0.4] transition-all duration-500 hover:brightness-100 drop-shadow-lg max-md:max-h-16 max-md:max-w-16"
             />
+          </div>
+        </div>
+      </section>
+      {/* CLIENT PROJECTS */}
+      <section className="max-w-[1200px] mx-auto">
+        <h2 className="text-4xl tracking-[-4px] pb-6">Client projects</h2>
+        <div className="grid  gap-8 content-center justify-center justify-items-center items-center px-4 ">
+          <HomeClientCarousel />
+          <div className="grid">
+            <div className="pb-6 max-sm:text-justify">
+              <h3 className=" text-xl max-sm:text-base">
+                Over the course of my freelance work, I&apos;ve successfully
+                delivered a variety of projects for clients, each with its own
+                unique requirements:
+              </h3>
+              <ul className="grid gap-3 p-2">
+                <li>
+                  <h4 className="text-lg max-sm:text-sm">Landing Pages:</h4>
+                  <p className="max-sm:text-xs">
+                    I developed two landing pages—one using HTML, CSS, and
+                    JavaScript, and another using React with component-based
+                    styling. Both projects were designed to be responsive and
+                    visually engaging, providing my clients with user-friendly,
+                    conversion-focused pages.
+                  </p>
+                </li>
+                <li>
+                  <h4 className="text-lg max-sm:text-sm">
+                    Multi-Page Website:
+                  </h4>
+                  <p className="max-sm:text-xs">
+                    I built a comprehensive website with multiple subpages,
+                    using HTML, CSS, and JavaScript. The focus was on clarity,
+                    ease of navigation, and delivering a professional appearance
+                    for the client&apos;s business needs.
+                  </p>
+                </li>
+                <li>
+                  <h4 className="text-lg max-sm:text-sm">
+                    Full-Stack Blog Website:
+                  </h4>
+                  <p className="max-sm:text-xs">
+                    I created a full-stack website using Next.js and Tailwind
+                    CSS, which included a fully integrated blog with a CMS. This
+                    allows my client to easily add and manage articles. I used
+                    Supabase to manage the backend and handle API requests,
+                    ensuring a smooth and scalable system.
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <Link
+              href="/about"
+              className="text-xl border-b-[1px] border-[#575858] py-2 px-4 justify-self-center w-fit max-[380px]:max-sm:text-lg text-center"
+            >
+              More about client projects
+            </Link>
           </div>
         </div>
       </section>
