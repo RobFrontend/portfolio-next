18f4bcc87373d1917cdcacc43be645e70f7b6e0d
RobFrontend <robert.grabowski97@gmail.com>
2024-10-21 16:51:47 +0200

    hobby

--- app/page.js
+++ app/page.js
@@ -167,7 +167,7 @@ export default function Home() {
       <section className="max-w-[1200px] mx-auto">
         <h2 className="text-4xl tracking-[-4px] pb-6">Hobby projects</h2>
         <div className="grid  gap-8 content-center justify-center justify-items-center items-center px-4 max-sm:gap-5">
-          <div className="grid grid-cols-4 ">
+          <div className="grid grid-cols-4 max-md:grid-cols-3 max-[425px]:grid-cols-2">
             <Image
               src={RetroGame}
               alt="hobby"
@@ -231,43 +231,34 @@ export default function Home() {
           </div>
           <div className="grid">
             <div className="pb-6 max-sm:text-justify">
-              <h3 className=" text-xl max-sm:text-base">
-                Over the course of my freelance work, I&apos;ve successfully
-                delivered a variety of projects for clients, each with its own
-                unique requirements:
-              </h3>
-              <ul className="grid gap-3 p-2">
+              <ul className="grid gap-3">
                 <li>
-                  <h4 className="text-lg max-sm:text-sm">Landing Pages:</h4>
-                  <p className="max-sm:text-xs">
-                    I developed two landing pages—one using HTML, CSS, and
-                    JavaScript, and another using React with component-based
-                    styling. Both projects were designed to be responsive and
-                    visually engaging, providing my clients with user-friendly,
-                    conversion-focused pages.
+                  <h3 className="text-xl max-sm:text-lg">
+                    Versatile Project Development
+                  </h3>
+                  <p className="max-sm:text-sm">
+                    In my free time, I&apos;ve developed a wide range of
+                    projects that demonstrate my versatility and passion for
+                    learning new technologies. These projects include landing
+                    pages, multi-page websites, small e-commerce shops, user
+                    input fields, and a full-stack blog with a custom CMS.
+                    I&apos;ve also worked on API integrations, both building my
+                    own and consuming external APIs. One of my recent smaller
+                    projects involved creating a Spotify cover component.
                   </p>
                 </li>
                 <li>
-                  <h4 className="text-lg max-sm:text-sm">
-                    Multi-Page Website:
-                  </h4>
-                  <p className="max-sm:text-xs">
-                    I built a comprehensive website with multiple subpages,
-                    using HTML, CSS, and JavaScript. The focus was on clarity,
-                    ease of navigation, and delivering a professional appearance
-                    for the client&apos;s business needs.
-                  </p>
-                </li>
-                <li>
-                  <h4 className="text-lg max-sm:text-sm">
-                    Full-Stack Blog Website:
-                  </h4>
-                  <p className="max-sm:text-xs">
-                    I created a full-stack website using Next.js and Tailwind
-                    CSS, which included a fully integrated blog with a CMS. This
-                    allows my client to easily add and manage articles. I used
-                    Supabase to manage the backend and handle API requests,
-                    ensuring a smooth and scalable system.
+                  <h3 className="text-xl max-sm:text-lg">
+                    Diverse Technology Stack
+                  </h3>
+                  <p className="max-sm:text-sm">
+                    These projects span a variety of technologies, from HTML,
+                    CSS, and JavaScript, to more advanced tools like React,
+                    Next.js with Tailwind CSS, Supabase, and even a project in
+                    Node.js. Some of my projects also incorporate local storage
+                    to enhance user experience. Through these projects, I
+                    continuously challenge myself to improve, both as a
+                    developer and a creator.
                   </p>
                 </li>
               </ul>
