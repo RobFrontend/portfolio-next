754414d949d5c8e0959363e781e4c5f2aec993e5
RobFrontend <robert.grabowski97@gmail.com>
2024-10-24 01:38:03 +0200

    client page

--- app/clientprojects/page.js
+++ app/clientprojects/page.js
@@ -56,7 +56,7 @@ function Page() {
           </div>
 
           <div className="grid ">
-            <div className="flex gap-8 pb-8 pt-2 content-center justify-center flex-wrap max-sm:gap-4">
+            <div className="flex gap-8 pb-6 pt-2 content-center justify-center flex-wrap max-sm:gap-4">
               <Image
                 src={AboutNext}
                 alt="logo"
@@ -138,7 +138,7 @@ function Page() {
             <Image
               src={JeszczeStrona}
               alt="project"
-              className="max-w-[600px] max-md:max-w-[90%]"
+              className="max-w-[600px] max-md:max-w-[90%] hover:translate-y-[-6px] transition-all duration-500"
             />
           </div>
         </div>
@@ -161,7 +161,7 @@ function Page() {
           </div>
 
           <div className="grid ">
-            <div className="flex gap-8 pb-8 pt-2 content-center justify-center flex-wrap max-sm:gap-4">
+            <div className="flex gap-8 pb-6 pt-2 content-center justify-center flex-wrap max-sm:gap-4">
               <Image
                 src={AboutReact}
                 alt="logo"
@@ -169,45 +169,29 @@ function Page() {
               />
             </div>
             <div className="p-4">
-              <h3 className="text-center text-xl">Server Side Rendering</h3>
+              <h3 className="text-center text-xl">
+                Landing Page for a Local Supplement Shop
+              </h3>
               <p className="text-justify">
-                This project{" "}
+                This{" "}
                 <Link
-                  href="https://www.jeszczestronaalbosto.pl/"
+                  href="https://www.odzywki-legionowo.pl/"
                   target="_blank"
                   className="border-b-[1px] border-[#575858] w-min"
                 >
-                  jeszczestronaalbosto.pl
+                  Biceps
                 </Link>{" "}
-                is a fully responsive, full-stack website built with Next.js,
-                Tailwind CSS, and Supabase, making it one of the most complex
-                projects I&apos;ve developed for a client. The website includes
-                five main pages: Home Page, About Me, Collab, Contact, and Blog.
-                It&apos;s dedicated to book enthusiasts, with the blog serving
-                as a platform for book reviews.
+                project is a responsive landing page built in React using
+                styled-components and third-party libraries. Created for a local
+                supplement shop, the design is clean, user-friendly, and focused
+                on delivering essential information to the customer. It provides
+                clear details about the shop&apos;s products, contact
+                information, and location, ensuring that users can easily find
+                what they need. The project is fully responsive, offering a
+                smooth browsing experience on any device.
               </p>
             </div>
-            <div className="p-4">
-              <h3 className="text-center text-xl">Own CMS</h3>
 
-              <p className="text-justify">
-                To provide my client with full control over the blog content, I
-                created a custom CMS, hosted on a separate domain and accessible
-                after authentication. This allows the client to easily add and
-                manage new blog posts. The site also includes a notification
-                system using React Hot Toast, ensuring that the owner stays
-                updated with important actions on the site.
-              </p>
-            </div>
-            <div className="p-4">
-              <p className="text-justify">
-                Users can read reviews of their favorite books, explore book
-                publishing companies featured on the site, and use the contact
-                form to directly reach out to the site owner. This project
-                stands out for its complexity and attention to user experience,
-                combining a dynamic front-end with a powerful back-end.
-              </p>
-            </div>
             <Link
               href="https://www.odzywki-legionowo.pl/"
               target="_blank"
@@ -220,7 +204,7 @@ function Page() {
             <Image
               src={Biceps}
               alt="project"
-              className="max-w-[600px] max-md:max-w-[90%]"
+              className="max-w-[600px] max-md:max-w-[90%] hover:translate-y-[-6px] transition-all duration-500"
             />
           </div>
         </div>
@@ -243,7 +227,7 @@ function Page() {
           </div>
 
           <div className="grid ">
-            <div className="flex gap-8 pb-8 pt-2 content-center justify-center flex-wrap max-sm:gap-4">
+            <div className="flex gap-8 pb-6 pt-2 content-center justify-center flex-wrap max-sm:gap-4">
               <Image
                 src={AboutHtml}
                 alt="logo"
@@ -261,45 +245,30 @@ function Page() {
               />
             </div>
             <div className="p-4">
-              <h3 className="text-center text-xl">Server Side Rendering</h3>
+              <h3 className="text-center text-xl">
+                Landing Page for a 360° Photobooth Business
+              </h3>
               <p className="text-justify">
-                This project{" "}
+                This{" "}
                 <Link
-                  href="https://www.jeszczestronaalbosto.pl/"
+                  href="https://robson-fotobudka360.pl/"
                   target="_blank"
                   className="border-b-[1px] border-[#575858] w-min"
                 >
-                  jeszczestronaalbosto.pl
+                  robson-fotobudka360.pl
                 </Link>{" "}
-                is a fully responsive, full-stack website built with Next.js,
-                Tailwind CSS, and Supabase, making it one of the most complex
-                projects I&apos;ve developed for a client. The website includes
-                five main pages: Home Page, About Me, Collab, Contact, and Blog.
-                It&apos;s dedicated to book enthusiasts, with the blog serving
-                as a platform for book reviews.
+                landing page was crafted using HTML, CSS, and JavaScript for a
+                360° photobooth owner. The website embraces a colorful,
+                party-themed design, perfectly matching the nature of the
+                business. Fully responsive, it highlights key information such
+                as an &aspo;About Us&aspo; section, recommended event types for
+                the photobooth, customer reviews (connected to a free Google
+                API), and links to social media platforms like Instagram and
+                TikTok. The vibrant design creates a fun user experience while
+                ensuring the content is clear and easy to navigate.
               </p>
             </div>
-            <div className="p-4">
-              <h3 className="text-center text-xl">Own CMS</h3>
 
-              <p className="text-justify">
-                To provide my client with full control over the blog content, I
-                created a custom CMS, hosted on a separate domain and accessible
-                after authentication. This allows the client to easily add and
-                manage new blog posts. The site also includes a notification
-                system using React Hot Toast, ensuring that the owner stays
-                updated with important actions on the site.
-              </p>
-            </div>
-            <div className="p-4">
-              <p className="text-justify">
-                Users can read reviews of their favorite books, explore book
-                publishing companies featured on the site, and use the contact
-                form to directly reach out to the site owner. This project
-                stands out for its complexity and attention to user experience,
-                combining a dynamic front-end with a powerful back-end.
-              </p>
-            </div>
             <Link
               href="https://robson-fotobudka360.pl/"
               target="_blank"
@@ -312,7 +281,7 @@ function Page() {
             <Image
               src={Robson}
               alt="project"
-              className="max-w-[600px] max-md:max-w-[90%]"
+              className="max-w-[600px] max-md:max-w-[90%] hover:translate-y-[-6px] transition-all duration-500"
             />
           </div>
         </div>
